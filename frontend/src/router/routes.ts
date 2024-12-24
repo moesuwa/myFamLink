7c473dc672686d1ddbf4529afbc3caf314f8f249
suwamoe <suwa.moe@dominosoft.co.jp>
2024-12-24 04:22:22 -0800

    reset repository

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -3,8 +3,7 @@ import type { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('src/ui/layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('src/ui/pages/IndexPage.vue') }],
+    component: () => import('src/ui/pages/IndexPage.vue')
   },
 
   // Always leave this as last one,
--- frontend/src/ui/pages/IndexPage.vue
+++ frontend/src/ui/pages/IndexPage.vue
@@ -1,43 +1,4 @@
+<script setup lang="ts"></script>
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+  <div />
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'src/ui/components/models';
-import ExampleComponent from 'src/ui/components/ExampleComponent.vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200
-});
-</script>
