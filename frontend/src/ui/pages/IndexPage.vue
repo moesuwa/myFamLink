2cfaa3227019a40a8b179782762de79d844c6159
suwamoe <suwa.moe@dominosoft.co.jp>
2024-12-24 04:09:52 -0800

    move file

--- frontend/src/ui/pages/IndexPage.vue
+++ frontend/src/ui/pages/IndexPage.vue
@@ -11,8 +11,8 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import type { Todo, Meta } from 'src/ui/components/models';
+import ExampleComponent from 'src/ui/components/ExampleComponent.vue';
 
 const todos = ref<Todo[]>([
   {
