5e62a18fad1b5cc956e0b66292520f74525f8c51
suwamoe <suwa.moe@dominosoft.co.jp>
2024-12-24 03:18:02 -0800

    edit i18n

--- frontend/src/boot/i18n.ts
+++ frontend/src/boot/i18n.ts
@@ -5,7 +5,7 @@ import messages from 'src/i18n';
 
 export type MessageLanguages = keyof typeof messages;
 // Type-define 'en-US' as the master schema for the resource
-export type MessageSchema = typeof messages['en-US'];
+export type MessageSchema = typeof messages['ja'];
 
 // See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
 /* eslint-disable @typescript-eslint/no-empty-object-type */
@@ -21,13 +21,28 @@ declare module 'vue-i18n' {
 }
 /* eslint-enable @typescript-eslint/no-empty-object-type */
 
-export default defineBoot(({ app }) => {
   const i18n = createI18n<{ message: MessageSchema }, MessageLanguages>({
-    locale: 'en-US',
+    locale: 'ja',
     legacy: false,
     messages,
   });
 
+export default defineBoot(({ app }) => {
   // Set i18n instance on app
   app.use(i18n);
 });
+
+// useI18nは、vue-i18nにも定義が存在するが、vue-i18nはsetupコンテキストからしか呼び出せず、
+// 初期化中のawait処理の後で呼び出すとエラーとなる。
+// それは不便なので、どこからでも呼び出せるよう事前定義にしておく。
+export function useI18n() {
+  const { t, te, d, n, ...globalApi } = i18n.global;
+
+  return {
+    t: t.bind(i18n),
+    d: d.bind(i18n),
+    te: te.bind(i18n),
+    n: n.bind(i18n),
+    ...globalApi,
+  };
+}
--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,5 +1,5 @@
-import enUS from './en-US';
+import ja from './ja';
 
 export default {
-  'en-US': enUS
+  ja,
 };
